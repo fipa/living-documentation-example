b74071d41d706de8c01438d884fd02e6352a0c88
FIPA <fernandopobletearrau@gmail.com>
2015-11-04 22:11:12 -0300

    Se muestra la imagen de los productos en la ficha y en el index

--- ScrumShop/app/helpers/products_helper.rb
+++ ScrumShop/app/helpers/products_helper.rb
@@ -1,2 +1,11 @@
 module ProductsHelper
+
+	def product_image(product)
+		image_tag(product.id.to_s + ".jpg", width: 300)
+	end
+
+	def product_thumbnail(product)
+		image_tag(product.id.to_s + ".jpg", width: 100)
+	end
+
 end
