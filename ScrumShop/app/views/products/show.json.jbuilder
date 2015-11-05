2731072ae71944f66515b9de44f5b485caa55473
FIPA <fernandopobletearrau@gmail.com>
2015-11-04 21:13:15 -0300

    Se crea scaffold inicial de productos

---
+++ ScrumShop/app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :description, :price, :stock, :category_id, :created_at, :updated_at
--- ScrumShop/db/schema.rb
+++ ScrumShop/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105000355) do
+ActiveRecord::Schema.define(version: 20151105001156) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.float    "price"
+    t.integer  "stock"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "login"
