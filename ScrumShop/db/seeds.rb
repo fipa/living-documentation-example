c7e4692cf69e8c981012d4bcc9f7268142bf6b08
FIPA <fernandopobletearrau@gmail.com>
2015-11-04 21:59:18 -0300

    Se crea seeds con usuarios, productos y categorias. Se agregan imagenes para productos inciales

--- ScrumShop/db/seeds.rb
+++ ScrumShop/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(login: 'fpoblete', email: 'ferpobletea@gmail.com', first_name: 'Fernando', last_name: 'Poblete', password: 'estaEsMyPassword')
+User.create(login: 'scrumDayChile', email: 'scrumDayChile@scrum.com', first_name: 'Scrum', last_name: 'Day Chile', password: 'scrumDayChile')
+
+Category.create(name: 'Articulos', description: 'Articulos de escritorio y para dinamicas de equipo')
+Category.create(name: 'Libros', description: 'Libros sobre temas de Agilidad')
+Category.create(name: 'Poleras', description: 'Poleras con mensajes y dibujos agiles')
+
+articles_category_id = Category.find_by_name("Articulos").id
+books_category_id = Category.find_by_name("Libros").id
+t_shirts_category_id = Category.find_by_name("Poleras").id
+
+Product.create(name: 'Scrum Post Its', price: 1200, stock: 100, category_id: articles_category_id,
+	description: 'Imprescindible para cualquier Planning, Grooming, Retrospectiva, con pegamento ultra sticky')
+
+Product.create(name: 'Scrum Quick Reference', price: 10000, stock: 20, category_id: books_category_id,
+	description: 'Compilado con ayuda memoria para los principales conceptos de Scrum')
+
+Product.create(name: 'Planning Poker Cards', price: 3000, stock: 50, category_id: articles_category_id,
+	description: 'Cartas para jugar al Planning Poker')
+
+Product.create(name: 'Agile Rocks T-Shirt', price: 8000, stock: 30, category_id: t_shirts_category_id,
+	description: 'En tallas 1,2,3,5,8,13')
+
